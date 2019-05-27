e19114a75962e5fa0efc854c7c05715db13df900
GD3 <GD3@DESKTOP-NUPJU7B>
2019-05-27 12:08:36 +0900

    MyBatis Connect add

--- src/main/java/org/june/w_apps/HomeController.java
+++ src/main/java/org/june/w_apps/HomeController.java
@@ -1,7 +1,12 @@
 package org.june.w_apps;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,11 +17,18 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	// Get, Post 방식 둘다 사용할때 
+	
+	@Autowired
+	SqlSession session;
+	
 	@RequestMapping("/select")
 	public String select() {
-		logger.info("Welcome home! The client locale is {}.", 1);
+		logger.info("Welcome home! The client locale is " + 1 + ".");
+		List<HashMap<String, Object>> resultList = session.selectList("test.select");
+		logger.info("Count : {}", resultList.size());
 		return "home";
 	}
+
 	
 	@RequestMapping("/insert")
 	public String insert() {
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h1>
-	Hello world! GD~~~~~~~~  
+	Hello world! GD~~~~~~~~~~~~~~~~~~  
 </h1>
 
 </body>
